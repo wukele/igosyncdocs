67951d71a1eb7794b1e8fce8a229c38053524293
baratjan <baratjan@d33ba324-4695-7563-bdd0-45f5434176c5>
2010-07-21 05:47:12 +0000

    add thread function to share document

--- trunk/iGoSyncDocs/src/barrywei/igosyncdocs/gui/ShareWithGroupsDialog.java
+++ trunk/iGoSyncDocs/src/barrywei/igosyncdocs/gui/ShareWithGroupsDialog.java
@@ -76,7 +76,15 @@ public class ShareWithGroupsDialog extends JDialog {
 			this.frame = frame;
 			biz = AbstractFactory.createSyncDocsBizObject();
 			initComponents();
-			initNeededData();
+			new Thread(new Runnable() {
+				public void run() {
+					try {
+						initNeededData();
+					} catch (SyncDocsException e) {
+						JOptionPane.showMessageDialog(null, e.getMessage(),"IGoSyncDocs Error", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}).start();
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e.getMessage(),"IGoSyncDocs Error", JOptionPane.ERROR_MESSAGE);
 			System.exit(1);
