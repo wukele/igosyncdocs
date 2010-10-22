5a036b9d156f20ebf4d781aa641ba72f5ec39daf
baratjan <baratjan@d33ba324-4695-7563-bdd0-45f5434176c5>
2010-10-22 07:56:43 +0000

    add system tray function to main frame

--- iGoSyncDocs-2010-Beta/src/barrywei/igosyncdocsv2/gui/renderer/EntityTableHeaderRenderer.java
+++ iGoSyncDocs-2010-Beta/src/barrywei/igosyncdocsv2/gui/renderer/EntityTableHeaderRenderer.java
@@ -9,7 +9,6 @@ import java.awt.Component;
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
-import javax.swing.UIManager;
 import javax.swing.table.TableCellRenderer;
 
 /**
