27c644943b89c3b86842b093e1e376651ec91cdd
BarryWey@gmail.com <BarryWey@gmail.com@d33ba324-4695-7563-bdd0-45f5434176c5>
2011-04-06 08:21:53 +0000

    language file settings updated

--- iGoSyncDocs-2011/src/barrywey/igosyncdocs2011/resource/LanguageResource.java
+++ iGoSyncDocs-2011/src/barrywey/igosyncdocs2011/resource/LanguageResource.java
@@ -4,20 +4,38 @@
  */
 package barrywey.igosyncdocs2011.resource;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.ResourceBundle;
+
+import barrywey.igosyncdocs2011.bean.SystemRuntime;
 
 /**
  * 
- *
- *
+ * 
+ * 
  * @author BarryWei
  * @version 1.0, Oct 12, 2010
  * @since JDK1.6
  */
 public class LanguageResource {
-	
+
 	public static String getStringValue(String key) {
-		return ResourceBundle.getBundle("barrywey.igosyncdocs2011.resource.lang.LanguageResource",Locale.getDefault()).getString(key);
+//		File configFile = new File(SystemRuntime.Settings.Config_File_Path);
+//		Properties pro = new Properties();
+//		try {
+//			pro.load(new FileInputStream(configFile));
+//		} catch (FileNotFoundException e) {
+//		} catch (IOException e) {
+//		}
+//		String defaultLanguage = pro.getProperty("default.language");
+		return ResourceBundle.getBundle(			
+				"barrywey.igosyncdocs2011.resource.lang.LanguageResource",
+				Locale.getDefault(), LanguageResource.class.getClassLoader())
+				.getString(key);
 	}
 }
